5578ee42324a8f30e1edfd801229f2b72a441508
Sergey Shlyakhov <sergey.schlyakhov@ya.ru>
2022-08-20 10:20:26 +0300

    init repo

---
+++ src/main/java/com/arm/CreditManagerWorkstation/CreditManagerWorkstationApplication.java
@@ -0,0 +1,13 @@
+package com.arm.CreditManagerWorkstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagerWorkstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditManagerWorkstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arm/CreditManagerWorkstation/CreditManagerWorkstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arm.CreditManagerWorkstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagerWorkstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
